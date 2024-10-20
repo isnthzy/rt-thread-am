17c61871656d6380a793716e128e4f79ea7a1e34
wangxin-wsl2 <isnthzy@gmail.com>
2024-10-20 14:23:38 +0800

    add: uart rx scan

--- bsp/abstract-machine/src/uart.c
+++ bsp/abstract-machine/src/uart.c
@@ -11,6 +11,7 @@
 #include <rtthread.h>
 #include <am.h>
 #include <klib.h>
+#include <klib-macros.h>
 
 #define UART_DEFAULT_BAUDRATE 115200
 
@@ -37,8 +38,25 @@ static int _uart_putc(struct rt_serial_device *serial, char c) {
 }
 
 static int _uart_getc(struct rt_serial_device *serial) {
+  #define UART_RX_ADDR =
   static const char *p = "help\ndate\nversion\nfree\nps\npwd\nls\nmemtrace\nmemcheck\nutest_list\n";
-  return (*p != '\0' ? *(p ++) : -1);
+  int ret;
+  if(*p != '\0'){
+    ret = *(p ++);
+  }else{
+    while (1)
+    {
+      char ch = io_read(AM_UART_RX).data;
+      if (ch == (char)-1){
+        ret = -1;
+        break;
+      }else{
+        ret = ch;
+        break;
+      }
+    }
+  }
+  return ret;
 }
 
 const struct rt_uart_ops _uart_ops = {
